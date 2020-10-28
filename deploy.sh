0d58e02196985cec5bc8e6dfc18d32bc16407948
Colt Bauman <cabauman@calmid.com>
2020-10-28 17:30:42 +0900

    fix: deploy fails
    - Happens when the default region isn't ap-northeast-2

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker build -t kidsloop-beta-user .
 docker tag kidsloop-beta-user:latest 494634321140.dkr.ecr.ap-northeast-2.amazonaws.com/kidsloop-beta-user:latest
 docker push 494634321140.dkr.ecr.ap-northeast-2.amazonaws.com/kidsloop-beta-user:latest
 sleep 5
-aws ecs update-service --service arn:aws:ecs:ap-northeast-2:494634321140:service/beta-hub/kidsloop-beta-user --force-new-deployment --cluster beta-hub
+aws ecs update-service --service arn:aws:ecs:ap-northeast-2:494634321140:service/beta-hub/kidsloop-beta-user --force-new-deployment --cluster beta-hub --region ap-northeast-2
