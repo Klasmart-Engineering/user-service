a968ab8b35a622a5f30213f97a80ffa8150e8b3d
Lucio Mollinedo <luciom@vsisoft.com>
2020-10-28 06:28:49 -0600

    update addOrganization mutation functionality

--- src/entities/organization.ts
+++ src/entities/organization.ts
@@ -74,6 +74,13 @@ export class Organization {
     public __req__: EntityId | null = {}
 
     @Column({nullable: true})
+    public shortCode?: string
+
+    // pass arguments for validation of UniqueOnDatabase constraint 
+    // columns on updating the entity
+    public __req__: EntityId | null = {}
+
+    @Column({nullable: true})
     public logoKey?: string
 
     public async logo() {
