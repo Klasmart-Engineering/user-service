f7ec81f88ff35b0c9df53073c7b23d1917c7946b
Colt Bauman <cabauman@calmid.com>
2020-11-05 10:09:25 +0900

    add a few role and user tests

---
+++ tests/integration/model.role.test.ts
@@ -0,0 +1,41 @@
+import { expect } from "chai";
+import { Connection } from "typeorm"
+import { createTestClient } from "apollo-server-testing"
+import { Model } from "../../src/model";
+import { createTestConnection } from "../utils/testConnection";
+import { createServer } from "../../src/utils/createServer";
+import { Role } from "../../src/entities/role";
+
+const GET_ROLES = `
+    query getRoles {
+        roles {
+            role_id
+            role_name
+        }
+    }
+`;
+
+describe("getRoles", () => {
+    let connection: Connection;
+
+    before(async () => {
+        connection = await createTestConnection();
+    });
+    
+    after(async () => {
+        await connection?.close();
+    });
+
+    it("should get roles", async () => {
+        const server = await createServer(new Model(connection), () => { });
+        const { query } = createTestClient(server);
+
+        const res = await query({
+            query: GET_ROLES,
+        });
+
+        expect(res.errors).to.be.undefined;
+        const roles = res.data.roles as Role[];
+        expect(roles).to.exist;
+    });
+});
--- tests/integration/model.user.test.ts
+++ tests/integration/model.user.test.ts
@@ -7,22 +7,32 @@ import { Model } from "../../src/model";
 import { createTestConnection } from "../utils/testConnection";
 import { createServer } from "../../src/utils/createServer";
 
-let connection: Connection;
-
-before(async () => {
-    connection = await createTestConnection();
-});
-
-after(async () => {
-    await connection?.close();
-});
-
 const NEW_USER = `
     mutation newUser(
         $user_name: String
         $email: String
         $avatar: String) {
         newUser(
+            user_name: $user_name
+            email: $email
+            avatar: $avatar
+        ) {
+            user_id
+            user_name
+            email
+            avatar
+        }
+    }
+`;
+
+const SET_USER = `
+    mutation setUser(
+        $user_id: ID!
+        $user_name: String
+        $email: String
+        $avatar: String) {
+        user(
+            user_id: $user_id
             user_name: $user_name
             email: $email
             avatar: $avatar
@@ -46,58 +56,148 @@ const GET_USERS = `
     }
 `;
 
-describe("newUser", () => {
-    it("should create a new user", async () => {
-        const user = {
-            user_name: faker.internet.userName(),
-            email: faker.internet.email(),
-        };
+const GET_USER = `
+    query getUser($user_id: ID!) {
+        user(user_id: $user_id) {
+            user_id
+            user_name
+            email
+            avatar
+        }
+    }
+`;
 
-        const server = await createServer(new Model(connection), () => { });
-        const { mutate } = createTestClient(server);
+describe("model.user", () => {
+    let connection: Connection;
 
-        const res = await mutate({
-            mutation: NEW_USER,
-            variables: user,
-        });
-
-        console.log(res);
-        console.log(res.errors);
-        expect(res.errors).to.be.undefined;
-        expect(res.data.newUser).to.include(user);
-
-        const dbUser = await User.findOne({ where: { email: user.email } });
-
-        expect(dbUser).to.exist;
-        expect(dbUser!.user_name).to.equal(user.user_name);
-        expect(dbUser!.avatar).to.be.null;
+    before(async () => {
+        connection = await createTestConnection();
     });
-});
 
-describe("getUsers", () => {
-    it("should get users", async () => {
-        const user = {
-            user_name: faker.internet.userName(),
-            email: faker.internet.email(),
-        };
+    after(async () => {
+        await connection?.close();
+    });
 
-        const server = await createServer(new Model(connection), () => { });
-        const { query, mutate } = createTestClient(server);
+    describe("newUser", () => {
+        it("should create a new user", async () => {
+            const user = {
+                user_name: faker.internet.userName(),
+                email: faker.internet.email(),
+            };
 
-        await mutate({
-            mutation: NEW_USER,
-            variables: user,
+            const server = await createServer(new Model(connection), () => { });
+            const { mutate } = createTestClient(server);
+
+            const res = await mutate({
+                mutation: NEW_USER,
+                variables: user,
+            });
+
+            expect(res.errors).to.be.undefined;
+            expect(res.data.newUser).to.include(user);
+
+            const dbUser = await User.findOne({ where: { email: user.email } });
+
+            expect(dbUser).to.exist;
+            expect(dbUser!.user_name).to.equal(user.user_name);
+            expect(dbUser!.avatar).to.be.null;
         });
+    });
 
-        const res = await query({
-            query: GET_USERS,
+    describe("setUser", () => {
+        it("should modify an existing user", async () => {
+            const originalUser = {
+                user_name: faker.internet.userName(),
+                email: faker.internet.email(),
+                avatar: "my avatar",
+            };
+
+            const server = await createServer(new Model(connection), () => { });
+            const { mutate } = createTestClient(server);
+
+            await mutate({
+                mutation: NEW_USER,
+                variables: originalUser,
+            });
+
+            let dbUser = await User.findOne({ where: { email: originalUser.email } });
+
+            const modifiedUser = {
+                user_id: dbUser!.user_id,
+                user_name: faker.internet.userName(),
+                email: faker.internet.email(),
+                avatar: "my new avatar",
+            };
+
+            const res = await mutate({
+                mutation: SET_USER,
+                variables: modifiedUser,
+            });
+
+            expect(res.errors).to.be.undefined;
+            expect(res.data.user).to.include(modifiedUser);
+
+            dbUser = await User.findOne(dbUser!.user_id);
+
+            expect(dbUser!.email).to.equal(modifiedUser.email);
+            expect(dbUser!.user_name).to.equal(modifiedUser.user_name);
+            expect(dbUser!.avatar).to.equal(modifiedUser.avatar);
         });
+    });
 
-        console.log(res);
-        console.log(res.errors);
-        expect(res.errors).to.be.undefined;
-        const users = res.data.users as User[];
-        expect(users).to.exist;
-        expect(users[users.length-1]).to.include(user);
+    describe("getUsers", () => {
+        it("should get users", async () => {
+            const user = {
+                user_name: faker.internet.userName(),
+                email: faker.internet.email(),
+            };
+
+            const server = await createServer(new Model(connection), () => { });
+            const { query, mutate } = createTestClient(server);
+
+            await mutate({
+                mutation: NEW_USER,
+                variables: user,
+            });
+
+            const res = await query({
+                query: GET_USERS,
+            });
+
+            expect(res.errors).to.be.undefined;
+            const users = res.data.users as User[];
+            expect(users).to.exist;
+            expect(users[users.length - 1]).to.include(user);
+        });
+    });
+
+    describe("getUser", () => {
+        it("should get user by ID", async () => {
+            const user = {
+                user_name: faker.internet.userName(),
+                email: faker.internet.email(),
+                avatar: "my avatar",
+            };
+
+            const server = await createServer(new Model(connection), () => { });
+            const { query, mutate } = createTestClient(server);
+
+            await mutate({
+                mutation: NEW_USER,
+                variables: user,
+            });
+
+            let dbUser = await User.findOneOrFail({ where: { email: user.email } });
+
+            const res = await query({
+                query: GET_USER,
+                variables: { user_id: dbUser.user_id },
+            });
+
+            expect(res.errors).to.be.undefined;
+            const returnedUser = res.data.user as User;
+            expect(returnedUser).to.exist;
+            expect(returnedUser).to.include(user);
+        });
     });
 });
