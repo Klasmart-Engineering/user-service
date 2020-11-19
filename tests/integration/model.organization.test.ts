6e35e6f6d5705883996d8ca591d45929607dfc26
emfg <emfg@calmid.com>
2020-11-19 16:11:40 +0100

    👮‍♂️  Add integration tests for resetDefaultRolesPermissions mutation

--- tests/integration/model.organization.test.ts
+++ tests/integration/model.organization.test.ts
@@ -4,6 +4,7 @@ import { Model } from "../../src/model";
 import { createTestConnection } from "../utils/testConnection";
 import { createServer } from "../../src/utils/createServer";
 import { Organization } from "../../src/entities/organization";
+import { Role } from "../../src/entities/role";
 import { createOrganization } from "../utils/operations/userOps";
 import { createUserJoe } from "../utils/testEntities";
 import { accountUUID } from "../../src/entities/user";
@@ -28,6 +29,21 @@ const GET_ORGANIZATION = `
     }
 `;
 
+const RESET_ORGANIZATION_ROLES_PERMISSIONS = `
+    mutation resetOrganizationRolesPermissions($organization_id: ID!) {
+        organization(organization_id: $organization_id) {
+            resetDefaultRolesPermissions {
+                role_id
+                role_name
+                permissions {
+                    permission_name
+                    role_id
+                }
+            }
+        }
+    }
+`;
+
 describe("model.organization", () => {
     let connection: Connection;
     let testClient: ApolloServerTestClient;
@@ -37,7 +53,7 @@ describe("model.organization", () => {
         const server = createServer(new Model(connection));
         testClient = createTestClient(server);
     });
-    
+
     after(async () => {
         await connection?.close();
     });
@@ -50,12 +66,12 @@ describe("model.organization", () => {
         context("when none", () => {
             it("should return an empty array", async () => {
                 const { query } = testClient;
-    
+
                 const res = await query({
                     query: GET_ORGANIZATIONS,
                     headers: { authorization: JoeAuthToken },
                 });
-    
+
                 expect(res.errors, res.errors?.toString()).to.be.undefined;
                 const organizations = res.data?.organizations as Organization[];
                 expect(organizations).to.exist;
@@ -71,12 +87,12 @@ describe("model.organization", () => {
 
             it("should return an array containing one organization", async () => {
                 const { query } = testClient;
-    
+
                 const res = await query({
                     query: GET_ORGANIZATIONS,
                     headers: { authorization: JoeAuthToken },
                 });
-    
+
                 expect(res.errors, res.errors?.toString()).to.be.undefined;
                 const organizations = res.data?.organizations as Organization[];
                 expect(organizations).to.exist;
@@ -89,17 +105,17 @@ describe("model.organization", () => {
         beforeEach(async () => {
             await connection.synchronize(true);
         });
-    
+
         context("when none", () => {
             it("should return null", async () => {
                 const { query } = testClient;
-        
+
                 const res = await query({
                     query: GET_ORGANIZATION,
                     variables: { organization_id: accountUUID() },
                     headers: { authorization: JoeAuthToken },
                 });
-        
+
                 expect(res.errors, res.errors?.toString()).to.be.undefined;
                 expect(res.data?.organization).to.be.null;
             });
@@ -115,17 +131,169 @@ describe("model.organization", () => {
 
             it("should return an array containing one organization", async () => {
                 const { query } = testClient;
-        
+
                 const res = await query({
                     query: GET_ORGANIZATION,
                     variables: { organization_id: organization.organization_id },
                     headers: { authorization: JoeAuthToken },
                 });
-        
+
                 expect(res.errors, res.errors?.toString()).to.be.undefined;
                 const gqlOrganization = res.data?.organization as Organization;
                 expect(gqlOrganization).to.exist;
                 expect(organization).to.include(gqlOrganization);
+            });
+        });
+    });
+
+    describe("resetDefaultRolesPermissions", () => {
+        beforeEach(async () => {
+            await connection.synchronize(true);
+        });
+
+        const roleInfoFunc =  function (role: any) {
+          return { role_id: role.role_id, role_name: role.role_name }
+        };
+        const permissionInfoFunc =  function (permission: any) {
+          return { permission_name: permission.permission_name, role_id: permission.role_id }
+        };
+
+        context("when updated default permissions exists", () => {
+            let organization: Organization;
+
+            beforeEach(async () => {
+                const user = await createUserJoe(testClient);
+                organization = await createOrganization(testClient, user.user_id);
+            });
+
+            it("does not modify the default roles permissions", async () => {
+                const { mutate } = testClient;
+                const dbRoles = await organization.roles || []
+                let dbPermissions = []
+                expect(dbRoles).not.to.be.empty;
+
+                for(const role of dbRoles){
+                  const permissions = await role.permissions || [];
+
+                  expect(permissions).not.to.be.empty
+                  dbPermissions.push(...permissions.map(permissionInfoFunc))
+                }
+
+                const res = await mutate({
+                    mutation: RESET_ORGANIZATION_ROLES_PERMISSIONS,
+                    variables: { organization_id: organization.organization_id },
+                    headers: {
+                        authorization: JoeAuthToken,
+                    },
+                });
+
+                organization = await Organization.findOneOrFail(organization.organization_id);
+                expect(dbRoles).not.to.be.empty;
+
+                expect(res.errors, res.errors?.toString()).to.be.undefined;
+                const gqlRoles = res.data?.organization?.resetDefaultRolesPermissions as Role[];
+                expect(gqlRoles.map(roleInfoFunc)).to.deep.equal(dbRoles?.map(roleInfoFunc));
+                let resetPermissions = []
+
+                for(const role of gqlRoles){
+                  const permissions = await role.permissions || [];
+
+                  expect(permissions).not.to.be.empty
+                  resetPermissions.push(...permissions?.map(permissionInfoFunc))
+                }
+
+                expect(dbPermissions).to.deep.members(resetPermissions)
+            });
+        });
+
+        context("when updated default permissions does not exists", () => {
+            let organization: Organization;
+
+            beforeEach(async () => {
+                const user = await createUserJoe(testClient);
+                organization = await createOrganization(testClient, user.user_id);
+                const roles = await organization.roles || []
+                await connection.manager.remove(roles)
+            });
+
+            it("does not create any default roles permissions", async () => {
+                const { mutate } = testClient;
+                organization = await Organization.findOneOrFail(organization.organization_id);
+                let dbRoles = await organization.roles || []
+                expect(dbRoles).to.be.empty;
+
+                const res = await mutate({
+                    mutation: RESET_ORGANIZATION_ROLES_PERMISSIONS,
+                    variables: { organization_id: organization.organization_id },
+                    headers: {
+                        authorization: JoeAuthToken,
+                    },
+                });
+
+                organization = await Organization.findOneOrFail(organization.organization_id);
+                dbRoles = await organization.roles || []
+                expect(dbRoles).to.be.empty;
+
+                expect(res.errors, res.errors?.toString()).to.be.undefined;
+                const gqlRoles = res.data?.organization?.resetDefaultRolesPermissions as Role[];
+                expect(gqlRoles).to.be.empty;
+            });
+        });
+
+        context("when outdated default permissions exists", () => {
+            let organization: Organization;
+
+            beforeEach(async () => {
+                const user = await createUserJoe(testClient);
+                organization = await createOrganization(testClient, user.user_id);
+            });
+
+            it("updates the default roles permissions", async () => {
+                const { mutate } = testClient;
+                let dbRoles = await organization.roles || []
+                let defaultPermissions = []
+                expect(dbRoles).not.to.be.empty;
+
+                for(const role of dbRoles){
+                  const permissions = await role.permissions || [];
+
+                  defaultPermissions.push(...permissions.map(permissionInfoFunc))
+                  await connection.manager.remove(permissions);
+                }
+
+                organization = await Organization.findOneOrFail(organization.organization_id);
+                dbRoles = await organization.roles || []
+                for(const role of dbRoles){
+                  const permissions = await role.permissions || [];
+
+                  expect(permissions).to.be.empty;
+                }
+
+                const res = await mutate({
+                    mutation: RESET_ORGANIZATION_ROLES_PERMISSIONS,
+                    variables: { organization_id: organization.organization_id },
+                    headers: {
+                        authorization: JoeAuthToken,
+                    },
+                });
+
+                organization = await Organization.findOneOrFail(organization.organization_id);
+                dbRoles = await organization.roles || []
+                expect(dbRoles).not.to.be.empty;
+
+                expect(res.errors, res.errors?.toString()).to.be.undefined;
+                const gqlRoles = res.data?.organization?.resetDefaultRolesPermissions as Role[];
+                expect(gqlRoles.map(roleInfoFunc)).to.deep.equal(dbRoles?.map(roleInfoFunc));
+                let resetPermissions = []
+
+                for(const role of gqlRoles){
+                  const permissions = await role.permissions || [];
+
+                  expect(permissions).not.to.be.empty
+                  resetPermissions.push(...permissions?.map(permissionInfoFunc))
+                }
+
+                expect(defaultPermissions).to.deep.members(resetPermissions)
             });
         });
     });
