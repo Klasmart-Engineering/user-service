4b4cd132e69c955d99acef968934ca2b94e6d536
emfg <emfg@calmid.com>
2020-12-01 15:39:33 +0100

    [KL-2810] 👷 Add super admin permissions

---
+++ tests/unit/permissions/superAdmin.test.ts
@@ -0,0 +1,22 @@
+import { expect } from "chai";
+import { latestPermissions } from "../../utils/latestPermissions";
+import { superAdminRole } from "../../../src/permissions/superAdmin";
+
+describe("superAdmintRole", () => {
+  const { permissions } = superAdminRole;
+
+  it("contains all the expected permissions", async () => {
+    const expectedPermissions = await latestPermissions();
+
+    for(const [ permissionCode , permissionInfo ] of expectedPermissions.entries()) {
+      if(permissionInfo.superAdmin) {
+        expect(permissions).to.include(permissionCode)
+      } else {
+        expect(permissions).not.to.include(permissionCode)
+      }
+
+    }
+
+  });
+});
+
