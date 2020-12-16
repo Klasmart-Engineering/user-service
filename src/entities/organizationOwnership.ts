836248701ed021630f318f257b70d7d4c04204c2
emfg <emfg@calmid.com>
2020-12-16 22:25:37 +0100

    [KL-3056] 🤝 Add organization ownership entity

---
+++ src/entities/organizationOwnership.ts
@@ -0,0 +1,36 @@
+import {
+    BaseEntity,
+    Column,
+    Entity,
+    getManager,
+    JoinColumn,
+    OneToOne,
+    PrimaryColumn,
+} from "typeorm";
+
+import { Organization } from './organization';
+import { Status } from "./status";
+import { User } from './user';
+
+@Entity()
+export class OrganizationOwnership extends BaseEntity {
+    @PrimaryColumn()
+    public user_id!: string
+
+    @OneToOne(() => User, user => user.user_id)
+    @JoinColumn({name: "user_id", referencedColumnName: "user_id"})
+    public user!: Promise<User>
+
+    @PrimaryColumn()
+    public organization_id!: string
+
+    @OneToOne(() => Organization, org => org.organization_id)
+    @JoinColumn({name: "organization_id", referencedColumnName: "organization_id"})
+    public organization!: Promise<Organization>
+
+    @Column({type: "enum", enum: Status, default: Status.ACTIVE})
+    public status! : Status
+
+    @Column({ type: 'timestamp', nullable: true})
+    public deleted_at?: Date
+}
---
+++ tests/unit/entities/organizationOwnership.test.ts
@@ -0,0 +1,106 @@
+import { expect, use } from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { Connection } from "typeorm";
+import { QueryFailedError } from "typeorm";
+
+import { createTestConnection } from "../../utils/testConnection";
+import { createUser } from "../../factories/user.factory";
+import { createOrganization } from "../../factories/organization.factory";
+import { Organization } from "../../../src/entities/organization";
+import { OrganizationOwnership } from "../../../src/entities/organizationOwnership";
+import { Status } from "../../../src/entities/status";
+import { User } from "../../../src/entities/user";
+
+use(chaiAsPromised);
+
+describe("OrganizationOwnership", () => {
+    let connection: Connection;
+    let manager : any;
+    let user : User;
+    let organization : Organization;
+
+    before(async () => {
+        connection = await createTestConnection();
+        manager = connection.manager
+    });
+
+    after(async () => {
+        await connection?.close();
+    });
+
+    beforeEach(async () => {
+        await connection.synchronize(true);
+        user = createUser()
+        organization = createOrganization(user);
+    });
+
+    describe(".new", () => {
+        context("when all details are correct", () => {
+            beforeEach(async () => {
+                await manager.save(user);
+                await manager.save(organization);
+            });
+
+            it("creates the OrganizationOwnership", async () => {
+                const ownership = new OrganizationOwnership()
+                ownership.user_id = user.user_id
+                ownership.organization_id = organization.organization_id
+                await manager.save(ownership)
+
+                const dbOwnership = await OrganizationOwnership.findOneOrFail({
+                    where: {
+                        organization_id: organization.organization_id,
+                        user_id: user.user_id
+                    }
+                })
+
+                const dbUser = await dbOwnership.user
+                expect(dbUser.user_id).to.eq(user.user_id)
+                const dbOrganization = await dbOwnership.organization
+                expect(dbOrganization.organization_id).to.eq(organization.organization_id)
+                expect(dbOwnership.status).to.eq(Status.ACTIVE)
+                expect(dbOwnership.deleted_at).to.be.null
+            });
+        });
+
+        context("when the organization does not exists", () => {
+            beforeEach(async () => {
+                await manager.save(user);
+            });
+
+            it("does not create the OrganizationOwnership", async () => {
+                const ownership = new OrganizationOwnership()
+                ownership.user_id = user.user_id
+                ownership.organization_id = organization.organization_id
+                const failFn = async () => { await manager.save(ownership) }
+
+                expect(failFn()).to.eventually.be.rejectedWith(QueryFailedError);
+
+                const dbOwnerships = await OrganizationOwnership.find({
+                    where: { user_id: user.user_id }
+                })
+                expect(dbOwnerships).to.be.empty
+            });
+        });
+
+        context("when the user does not exists", () => {
+            beforeEach(async () => {
+                await manager.save(organization);
+            });
+
+            it("does not create the OrganizationOwnership", async () => {
+                const ownership = new OrganizationOwnership()
+                ownership.user_id = user.user_id
+                ownership.organization_id = organization.organization_id
+                const failFn = async () => { await manager.save(ownership) }
+
+                expect(failFn()).to.eventually.be.rejectedWith(QueryFailedError);
+
+                const dbOwnerships = await OrganizationOwnership.find({
+                    where: { organization_id: organization.organization_id }
+                })
+                expect(dbOwnerships).to.be.empty
+            });
+        });
+    });
+});
