5dc62bd7ddc66d0070d1099159b1c8387381d288
Matthew Carey <mcarey@calmid.com>
2021-04-12 09:03:59 +0100

    Change user_id in editMembership to ID type

--- tests/utils/operations/organizationOps.ts
+++ tests/utils/operations/organizationOps.ts
@@ -140,7 +140,7 @@ const INVITE_USER = `
 `;
 
 const EDIT_MEMBERSHIP = `
-    mutation myMutation($organization_id: ID!, $user_id: String, $email:String, $phone: String, $given_name: String, $family_name: String, $date_of_birth: String, $username: String, $gender: String, $shortcode: String, $organization_role_ids: [ID!], $school_ids:[ID!] , $school_role_ids:[ID!], $alternate_email:String, $alternate_phone:String) {
+    mutation myMutation($organization_id: ID!, $user_id: ID, $email:String, $phone: String, $given_name: String, $family_name: String, $date_of_birth: String, $username: String, $gender: String, $shortcode: String, $organization_role_ids: [ID!], $school_ids:[ID!] , $school_role_ids:[ID!], $alternate_email:String, $alternate_phone:String) {
         organization(organization_id: $organization_id) {
             editMembership(user_id: $user_id, email: $email, phone:$phone, given_name: $given_name, family_name:$family_name,  date_of_birth:$date_of_birth, username: $username, gender: $gender, shortcode: $shortcode, organization_role_ids:$organization_role_ids, school_ids:$school_ids, school_role_ids:$school_role_ids, alternate_email:$alternate_email, alternate_phone:$alternate_phone){
                 user{
