fa96ab73f34e53cd88ea5f5de914e392eab6e518
olivermharrison <olivermharrison@gmail.com>
2021-07-05 16:55:44 +0100

    fix: user acceptance test now works

--- tests/acceptance/user.test.ts
+++ tests/acceptance/user.test.ts
@@ -6,16 +6,16 @@ import { before } from 'mocha'
 import { createTestConnection } from '../utils/testConnection'
 import { getAdminAuthToken } from '../utils/testConfig'
 import { loadFixtures } from '../utils/fixtures'
+import supertest from 'supertest'
 
 use(chaiAsPromised)
 
 const url = 'http://localhost:8080'
-const request = require('supertest')(url)
+const request = supertest(url)
 
 const ME = `
     query {
         me {
-            username
             email
         }
     }
@@ -23,18 +23,15 @@ const ME = `
 
 describe('acceptance.user', () => {
     let connection: Connection
-
     before(async () => {
         connection = await createTestConnection()
         await loadFixtures('users', connection)
     })
-
     after(async () => {
         await connection?.close()
     })
-
-    it('queries current user information successfully', (done) => {
-        request
+    it('queries current user information successfully', async () => {
+        const response = await request
             .post('/graphql')
             .set({
                 ContentType: 'application/json',
@@ -43,12 +40,8 @@ describe('acceptance.user', () => {
             .send({
                 query: ME,
             })
-            .expect(200)
-            .end((err: any, res: any) => {
-                if (err) return done(err)
-                expect(res.body.data.me.email).to.equal('joe@gmail.com')
-            })
 
-        done()
+        expect(response.status).to.eq(200)
+        expect(response.body.data.me.email).to.equal('joe@gmail.com')
     })
 })
