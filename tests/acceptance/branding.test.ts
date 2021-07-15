2df1e78c0279b88c36db697fbe4214ad39398b37
Matthew Carey <mcarey@calmid.com>
2021-07-15 14:35:23 +0000

    Merged in feature/UD-381-BE-Create-acceptance-test-for-deleting-branding-info-and-iamges-using-docker-compose (pull request #411)
    Acceptance tests for delete branding image and delete branding color
    
    * fix:Try to fix linux issues with upload test
    
    * fix: horrid cross platform solution
    
    * feat:delete branding image and color tests
    
    * fix: Remove last console.log
    
    * fix: remove redundant lines
    
    * fix: neatened the tests a bit
    
    * fix: remove some expect()s that are surplus
    
    
    Approved-by: Oliver Harrison
    Approved-by: Enrique M Figuerola Gomez

--- tests/acceptance/branding.test.ts
+++ tests/acceptance/branding.test.ts
@@ -7,8 +7,11 @@ import { expect, use } from 'chai'
 import { before } from 'mocha'
 
 import { createTestConnection } from '../utils/testConnection'
-import { getAdminAuthToken } from '../utils/testConfig'
+import { generateToken, getAdminAuthToken } from '../utils/testConfig'
 import { loadFixtures } from '../utils/fixtures'
+import { BrandingImageTag } from '../../src/types/graphQL/brandingImageTag'
+import { User } from '../../src/entities/user'
+import { userToPayload } from '../utils/operations/userOps'
 
 use(chaiAsPromised)
 
@@ -16,6 +19,8 @@ const url = 'http://localhost:8080'
 const request = supertest(url)
 const user_id = 'c6d4feed-9133-5529-8d72-1003526d1b13'
 const org_name = 'my-org'
+const user2_id = '16046442-75b8-4da5-b3df-aa53a70a13a1'
+const org2_name = 'my-org2'
 
 const GET_ORGANIZATION = `
     query myQuery($organizationId: ID!) {
@@ -53,6 +58,78 @@ const SET_BRANDING = `
         }
     }
 `
+const DELETE_BRANDING_IMAGE = `
+    mutation deleteOrganizationBrandingImage($organizationId: ID!, $brandingImageTag: BrandingImageTag!) {
+        deleteBrandingImage(
+            organizationId: $organizationId
+            type: $brandingImageTag
+        ) 
+    }
+`
+
+const DELETE_BRANDING_COLOR = `
+    mutation deleteOrganizationBrandingColor($organizationId: ID!) {
+        deleteBrandingColor(
+            organizationId: $organizationId
+        ) 
+    }
+`
+async function createOrg(user_id: string, token: string) {
+    return await request
+        .post('/graphql')
+        .set({
+            ContentType: 'application/json',
+            Authorization: token,
+        })
+        .send({
+            query: CREATE_ORGANIZATION,
+            variables: {
+                user_id,
+            },
+        })
+}
+
+async function getOrg(orgId: string, token: string) {
+    return await request
+        .post('/graphql')
+        .set({
+            ContentType: 'application/json',
+            Authorization: token,
+        })
+        .send({
+            query: GET_ORGANIZATION,
+            variables: {
+                organizationId: orgId,
+            },
+        })
+}
+
+async function setBranding(
+    orgId: string,
+    primaryColor: string,
+    imagePath: string,
+    token: string
+) {
+    const imageData = fs.readFileSync(imagePath)
+    const imageBuffer = Buffer.from(imageData)
+    return await request
+        .post('/graphql')
+        .set({
+            Authorization: token,
+        })
+        .field(
+            'operations',
+            JSON.stringify({
+                query: SET_BRANDING,
+                variables: {
+                    organizationId: orgId,
+                    primaryColor: primaryColor,
+                },
+            })
+        )
+        .field('map', JSON.stringify({ image: ['variables.organizationLogo'] }))
+        .attach('image', imageBuffer, imagePath)
+}
 
 describe('acceptance.branding', () => {
     let connection: Connection
@@ -60,29 +137,22 @@ describe('acceptance.branding', () => {
 
     before(async () => {
         connection = await createTestConnection()
-        await loadFixtures('users', connection)
     })
 
     after(async () => {
         await connection?.close()
     })
 
+    beforeEach(async () => {
+        await loadFixtures('users', connection)
+    })
+
     it('sets branding successfully', async () => {
         let organization_id: string = ''
 
         // create organization
-        const createOrgResponse = await request
-            .post('/graphql')
-            .set({
-                ContentType: 'application/json',
-                Authorization: getAdminAuthToken(),
-            })
-            .send({
-                query: CREATE_ORGANIZATION,
-                variables: {
-                    user_id,
-                },
-            })
+        const createOrgResponse = await createOrg(user_id, getAdminAuthToken())
+
         expect(createOrgResponse.status).to.eq(200)
 
         const createOrgData =
@@ -98,28 +168,12 @@ describe('acceptance.branding', () => {
             'fixtures/icon.png'
         )
 
-        const imageData = fs.readFileSync(imagePath)
-        const imageBuffer = Buffer.from(imageData)
-        const setBrandingResponse = await request
-            .post('/graphql')
-            .set({
-                Authorization: getAdminAuthToken(),
-            })
-            .field(
-                'operations',
-                JSON.stringify({
-                    query: SET_BRANDING,
-                    variables: {
-                        organizationId: orgId,
-                        primaryColor: primaryColor,
-                    },
-                })
-            )
-            .field(
-                'map',
-                JSON.stringify({ image: ['variables.organizationLogo'] })
-            )
-            .attach('image', imageBuffer, imagePath)
+        const setBrandingResponse = await setBranding(
+            orgId,
+            primaryColor,
+            imagePath,
+            getAdminAuthToken()
+        )
 
         expect(setBrandingResponse.status).to.eq(200)
 
@@ -128,18 +182,8 @@ describe('acceptance.branding', () => {
         expect(setBrandingData.iconImageURL).to.exist
 
         // get organization
-        const getOrgResponse = await request
-            .post('/graphql')
-            .set({
-                ContentType: 'application/json',
-                Authorization: getAdminAuthToken(),
-            })
-            .send({
-                query: GET_ORGANIZATION,
-                variables: {
-                    organizationId: orgId,
-                },
-            })
+        const getOrgResponse = await getOrg(orgId, getAdminAuthToken())
+
         expect(getOrgResponse.status).to.eq(200)
         expect(getOrgResponse.body.errors).to.not.exist
 
@@ -148,5 +192,157 @@ describe('acceptance.branding', () => {
         expect(getOrgData.organization_id).to.eq(orgId)
         const brandingData = getOrgData.branding
         expect(brandingData.primaryColor).to.equal(primaryColor)
+    })
+
+    it('deletes branding image successfully', async () => {
+        let organization_id: string = ''
+
+        const user2 = await connection
+            .getRepository(User)
+            .findOneOrFail({ user_id: user2_id })
+        const token = generateToken(userToPayload(user2))
+        // create organization
+        const createOrgResponse = await createOrg(user2_id, token)
+
+        expect(createOrgResponse.status).to.eq(200)
+
+        const createOrgData =
+            createOrgResponse.body.data.user.createOrganization
+        const orgId = createOrgData.organization_id
+        expect(orgId).to.exist
+        expect(createOrgData.organization_name).to.eq(org_name)
+
+        // set branding
+        const imagePath = path.resolve(
+            path.dirname(__filename),
+            '..',
+            'fixtures/icon.png'
+        )
+        const data = fs.readFileSync(imagePath)
+        const buffer = Buffer.from(data)
+        const setBrandingResponse = await setBranding(
+            orgId,
+            primaryColor,
+            imagePath,
+            token
+        )
+
+        expect(setBrandingResponse.status).to.eq(200)
+
+        const setBrandingData = setBrandingResponse.body.data.setBranding
+        expect(setBrandingData.primaryColor).to.eq(primaryColor)
+        expect(setBrandingData.iconImageURL).to.exist
+
+        // get organization
+        const getOrgResponse = await getOrg(orgId, token)
+
+        expect(getOrgResponse.status).to.eq(200)
+        const getOrgData = getOrgResponse.body.data.organization
+        const brandingData = await getOrgData.branding
+        expect(brandingData.iconImageURL).to.exist
+
+        const setDeleteBrandingImageResponse = await request
+            .post('/graphql')
+            .set({
+                ContentType: 'application/json',
+                Authorization: token,
+            })
+            .send({
+                query: DELETE_BRANDING_IMAGE,
+                variables: {
+                    organizationId: orgId,
+                    brandingImageTag: BrandingImageTag.ICON,
+                },
+            })
+
+        expect(setDeleteBrandingImageResponse.status).to.eq(200)
+
+        const getOrgResponse2 = await getOrg(orgId, token)
+
+        expect(getOrgResponse2.status).to.eq(200)
+        expect(getOrgResponse2.body.errors).to.not.exist
+
+        const getOrgData2 = getOrgResponse2.body.data.organization
+        expect(getOrgData2).to.exist
+        expect(getOrgData2.organization_id).to.eq(orgId)
+
+        const brandingData2 = await getOrgData2.branding
+        expect(brandingData2.primaryColor).to.equal(primaryColor)
+        expect(brandingData2.iconImageURL).to.not.exist
+    })
+
+    it('deletes branding color successfully', async () => {
+        let organization_id: string = ''
+
+        const user2 = await connection
+            .getRepository(User)
+            .findOneOrFail({ user_id: user2_id })
+        const token = generateToken(userToPayload(user2))
+        // create organization
+        const createOrgResponse = await createOrg(user2_id, token)
+
+        expect(createOrgResponse.status).to.eq(200)
+
+        const createOrgData =
+            createOrgResponse.body.data.user.createOrganization
+        const orgId = createOrgData.organization_id
+        expect(orgId).to.exist
+        expect(createOrgData.organization_name).to.eq(org_name)
+
+        // set branding
+        const imagePath = path.resolve(
+            path.dirname(__filename),
+            '..',
+            'fixtures/icon.png'
+        )
+
+        const setBrandingResponse = await setBranding(
+            orgId,
+            primaryColor,
+            imagePath,
+            token
+        )
+
+        expect(setBrandingResponse.status).to.eq(200)
+
+        const setBrandingData = setBrandingResponse.body.data.setBranding
+        expect(setBrandingData.primaryColor).to.eq(primaryColor)
+
+        // get organization
+
+        const getOrgResponse = await getOrg(orgId, token)
+
+        expect(getOrgResponse.status).to.eq(200)
+        const getOrgData = getOrgResponse.body.data.organization
+        const brandingData = await getOrgData.branding
+        expect(brandingData.primaryColor).to.equal(primaryColor)
+        expect(brandingData.iconImageURL).to.exist
+
+        const setDeleteBrandingColorResponse = await request
+            .post('/graphql')
+            .set({
+                ContentType: 'application/json',
+                Authorization: token,
+            })
+            .send({
+                query: DELETE_BRANDING_COLOR,
+                variables: {
+                    organizationId: orgId,
+                },
+            })
+        expect(setDeleteBrandingColorResponse.status).to.eq(200)
+
+        const getOrgResponse2 = await getOrg(orgId, token)
+
+        expect(getOrgResponse2.status).to.eq(200)
+        expect(getOrgResponse2.body.errors).to.not.exist
+
+        const getOrgData2 = getOrgResponse2.body.data.organization
+        expect(getOrgData2).to.exist
+        expect(getOrgData2.organization_id).to.eq(orgId)
+
+        const brandingData2 = await getOrgData2.branding
+        expect(brandingData2.primaryColor).to.equal(null)
+        expect(brandingData2.iconImageURL).to.exist
     })
 })
