c8ec0b8a005794247f7def6d32d2084906266a8e
Max Nicholson <max.nicholson@calmid.com>
2021-09-01 17:40:27 +0100

    test: enhance createUser
    - Allow passing arguments to initialise to
    - Add createAdminUser factory

--- tests/factories/user.factory.ts
+++ tests/factories/user.factory.ts
@@ -1,18 +1,47 @@
 import faker from 'faker'
 import { User } from '../../src/entities/user'
 
-const gender = ['Male', 'Female', 'Unspecified']
+const DEFAULT_GENDERS = ['Male', 'Female', 'Unspecified']
 
-export function createUser() {
+const ADMIN_EMAIL = 'joe@gmail.com'
+
+export function createUser({
+    given_name,
+    family_name,
+    email,
+    phone,
+    username,
+    date_of_birth,
+    gender,
+    alternate_email,
+    alternate_phone,
+}: Pick<
+    User,
+    | 'given_name'
+    | 'family_name'
+    | 'email'
+    | 'phone'
+    | 'username'
+    | 'date_of_birth'
+    | 'gender'
+    | 'alternate_email'
+    | 'alternate_phone'
+> = {}) {
     const user = new User()
 
-    user.given_name = faker.name.firstName()
-    user.family_name = faker.name.lastName()
-    user.email = faker.internet.email()
-    user.phone = faker.phone.phoneNumber('+44#######')
-    user.username = faker.internet.userName()
-    user.date_of_birth = '01-2018'
-    user.gender = faker.random.arrayElement(gender)
+    user.given_name = given_name ?? faker.name.firstName()
+    user.family_name = family_name ?? faker.name.lastName()
+    user.email = email ?? faker.internet.email()
+    user.phone = phone ?? faker.phone.phoneNumber('+44#######')
+    user.username = username ?? faker.internet.userName()
+    user.date_of_birth = date_of_birth ?? '01-2018'
+    user.gender = gender ?? faker.random.arrayElement(DEFAULT_GENDERS)
+    user.alternate_email = alternate_email
+    user.alternate_phone = alternate_phone
 
     return user
+}
+
+export function createAdminUser() {
+    return createUser({ email: ADMIN_EMAIL })
 }
