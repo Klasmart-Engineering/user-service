ede105bb56a06c49987374732561c1ee14456e5a
Nicolas Carelli <nicolasc@bluetrailsoft.com>
2021-10-07 08:45:28 -0300

    adding missing filter to reusable usersConnectionLoadersLoad func

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -10,6 +10,7 @@ import Dataloader from 'dataloader'
 import { Context } from '../main'
 import { UserConnectionNode } from '../types/graphQL/userConnectionNode'
 import { User } from '../entities/user'
+import { IEntityFilter } from '../utils/pagination/filtering'
 
 const typeDefs = gql`
     extend type Mutation {
@@ -265,7 +266,7 @@ export default function getDefault(
             Query: {
                 me: (_, _args, ctx, _info) => model.getMyUser(ctx),
                 usersConnection: (_parent, args, ctx: Context, info) => {
-                    usersConnectionLoadersLoad(ctx)
+                    usersConnectionLoadersLoad(ctx, args.filter)
                     return model.usersConnection(ctx, info, args)
                 },
                 userNode: (_parent, { id }, ctx: Context) => {
@@ -292,12 +293,12 @@ export default function getDefault(
     }
 }
 
-const usersConnectionLoadersLoad = (ctx: Context) => {
+const usersConnectionLoadersLoad = (ctx: Context, filter?: IEntityFilter) => {
     if (typeof ctx.loaders.usersConnection === 'undefined') {
         ctx.loaders.usersConnection = {
-            organizations: new Dataloader((keys) => orgsForUsers(keys)),
-            schools: new Dataloader((keys) => schoolsForUsers(keys)),
-            roles: new Dataloader((keys) => rolesForUsers(keys)),
+            organizations: new Dataloader((keys) => orgsForUsers(keys, filter)),
+            schools: new Dataloader((keys) => schoolsForUsers(keys, filter)),
+            roles: new Dataloader((keys) => rolesForUsers(keys, filter)),
         }
     }
 }
