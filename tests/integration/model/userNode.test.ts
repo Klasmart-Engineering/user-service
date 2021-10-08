88cb5a98b221307b53fe90d7ff0a2f9a39f6a4c2
Nicolas Carelli <nicolasc@bluetrailsoft.com>
2021-10-08 09:48:23 -0300

    Passing the scope to the dataloader init, adding permission checks tests

--- tests/integration/model/userNode.test.ts
+++ tests/integration/model/userNode.test.ts
@@ -286,4 +286,188 @@ describe('userNode', () => {
             expect(connection.logger.count).to.be.eq(1)
         })
     })
+
+    context('permissions', () => {
+        let organization: Organization
+        let user: User
+        let users: User[]
+        let schools: School[]
+        let organizations: Organization[]
+
+        beforeEach(async () => {
+            users = await User.save(createUsers(9))
+            user = users[0]
+            organizations = await Organization.save(createOrganizations(3))
+            organization = organizations[0]
+
+            await OrganizationMembership.save(
+                users.map((u, i) =>
+                    createOrganizationMembership({
+                        user: u,
+                        organization: organizations[i % 3],
+                    })
+                )
+            )
+
+            schools = await School.save([
+                createSchool(organizations[0]),
+                createSchool(organizations[1]),
+            ])
+
+            await SchoolMembership.save([
+                createSchoolMembership({
+                    user: users[0],
+                    school: schools[0],
+                }),
+                createSchoolMembership({
+                    user: users[3],
+                    school: schools[0],
+                }),
+                createSchoolMembership({
+                    user: users[1],
+                    school: schools[1],
+                }),
+            ])
+
+            await Class.save([
+                createClass(undefined, organizations[0], {
+                    students: [users[1], users[5], users[6]],
+                    teachers: [users[0]],
+                }),
+                createClass(undefined, organizations[1], {
+                    students: [users[2]],
+                    teachers: [users[1]],
+                }),
+            ])
+
+            // make at least one user in a different Org have the same email as `user`
+            users[8].email = user.email
+            await users[8].save()
+        })
+
+        context('admin', () => {
+            beforeEach(async () => {
+                // Make the User an admin
+                user.email = ADMIN_EMAIL
+                await user.save()
+            })
+            it('can view all Users', async () => {
+                const adminToken = generateToken(userToPayload(user))
+                for (const u of users) {
+                    const userNodeResponse = await userNode(
+                        testClient,
+                        { authorization: adminToken },
+                        u.user_id
+                    )
+
+                    expect(userNodeResponse).to.exist
+                }
+            })
+        })
+
+        context('non-admin', () => {
+            const addPermission = async ({
+                user,
+                organization,
+                permission,
+            }: {
+                user: User
+                organization: Organization
+                permission: PermissionName
+            }) => {
+                const role = await createRole(undefined, organization).save()
+                await OrganizationMembership.createQueryBuilder()
+                    .relation('roles')
+                    .of({
+                        user_id: user.user_id,
+                        organization_id: organization.organization_id,
+                    })
+                    .add(role)
+                await Role.createQueryBuilder()
+                    .relation('permissions')
+                    .of(role)
+                    .add(permission)
+            }
+
+            context('User with `view_users_40110`', () => {
+                beforeEach(
+                    async () =>
+                        await addPermission({
+                            user,
+                            organization,
+                            permission: PermissionName.view_users_40110,
+                        })
+                )
+                it('can view Users in the Organizations they belong to', async () => {
+                    const userNodeResponse = await userNode(
+                        testClient,
+                        {
+                            authorization: generateToken(userToPayload(user)),
+                        },
+                        users[3].user_id
+                    )
+                    expect(userNodeResponse).to.deep.equal(
+                        mapUserToUserConnectionNode(users[3])
+                    )
+                })
+            })
+            context('User with `view_my_school_users_40111', () => {
+                beforeEach(
+                    async () =>
+                        await addPermission({
+                            user,
+                            organization,
+                            permission:
+                                PermissionName.view_my_school_users_40111,
+                        })
+                )
+                it('can view Users in the Schools they belong to', async () => {
+                    const userNodeResponse = await userNode(
+                        testClient,
+                        {
+                            authorization: generateToken(userToPayload(user)),
+                        },
+                        users[3].user_id
+                    )
+                    expect(userNodeResponse).to.deep.equal(
+                        mapUserToUserConnectionNode(users[3])
+                    )
+                })
+            })
+            context('User with `view_my_users_40113`', () => {
+                beforeEach(
+                    async () =>
+                        await addPermission({
+                            user,
+                            organization,
+                            permission: PermissionName.view_my_users_40113,
+                        })
+                )
+                it('can view Users with the same email', async () => {
+                    const userNodeResponse = await userNode(
+                        testClient,
+                        {
+                            authorization: generateToken(userToPayload(user)),
+                        },
+                        users[8].user_id
+                    )
+                    expect(userNodeResponse).to.deep.equal(
+                        mapUserToUserConnectionNode(users[8])
+                    )
+                })
+            })
+            context('User with no "view_*_users" permission', () => {
+                it('can only see their own User', async () => {
+                    const userNodeResponse = await userNode(
+                        testClient,
+                        {
+                            authorization: generateToken(userToPayload(user)),
+                        },
+                        user.user_id
+                    )
+                    expect(userNodeResponse.id).to.deep.equal(user.user_id)
+                })
+            })
+        })
+    })
 })
