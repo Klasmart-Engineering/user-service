6a4b797e830ccfca60183a22d9bddadf2e55270f
james wilson <james.wilson@kidsloop.live>
2021-10-11 20:44:53 +0100

    testing newrelic deployment instrumentation

---
+++ scripts/monitor_deployment.sh
@@ -0,0 +1,17 @@
+# this script should calculate the 3 out of 4 key metrics of Software Delivery
+# https://thenewstack.io/4-ways-to-measure-your-software-delivery-performance/
+TIMESTAMP=date +%s 
+AUTHOR=git show -s --format='%ae' HASH
+curl -X POST "https://api.eu.newrelic.com/v2/applications/$NEWRELIC_APP_ID/deployments.json" \
+     -H "Api-Key:$NEWRELIC_API_KEY" \
+     -i \
+     -H "Content-Type: application/json" \
+     -d \
+'{
+  "deployment": {
+    "revision": "REVISION",
+    "description": "Added a deployments resource to the v2 API",
+    "user": "'+$AUTHOR+'",
+    "timestamp": "'+$TIMESTAMP+'"
+  }
+}'
