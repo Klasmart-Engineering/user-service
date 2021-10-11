9f6cb86db46d74adb1d6226530676066bd526169
james wilson <james.wilson@kidsloop.live>
2021-10-11 20:54:11 +0100

    updates to monitor script

--- scripts/monitor_deployment.sh
+++ scripts/monitor_deployment.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+
 # this script should calculate the 3 out of 4 key metrics of Software Delivery
 # https://thenewstack.io/4-ways-to-measure-your-software-delivery-performance/
-TIMESTAMP=date +%s 
-AUTHOR=git show -s --format='%ae' HASH
+
+
+TIMESTAMP="mytimestamp"
+AUTHOR="authors name"
+
+echo "https://api.eu.newrelic.com/v2/applications/$NEWRELIC_APP_ID/deployments.json"
+echo "Api-Key:$NEWRELIC_API_KEY"
+
 curl -X POST "https://api.eu.newrelic.com/v2/applications/$NEWRELIC_APP_ID/deployments.json" \
      -H "Api-Key:$NEWRELIC_API_KEY" \
      -i \
