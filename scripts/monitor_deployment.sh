0554acd2c02745a5e963b728346c7f1cc3512fca
james wilson <james.wilson@kidsloop.live>
2021-10-11 20:58:01 +0100

    debug monitor script

--- scripts/monitor_deployment.sh
+++ scripts/monitor_deployment.sh
@@ -10,16 +10,16 @@ AUTHOR="authors name"
 echo "https://api.eu.newrelic.com/v2/applications/$NEWRELIC_APP_ID/deployments.json"
 echo "Api-Key:$NEWRELIC_API_KEY"
 
-curl -X POST "https://api.eu.newrelic.com/v2/applications/$NEWRELIC_APP_ID/deployments.json" \
-     -H "Api-Key:$NEWRELIC_API_KEY" \
-     -i \
-     -H "Content-Type: application/json" \
-     -d \
-'{
-  "deployment": {
-    "revision": "REVISION",
-    "description": "Added a deployments resource to the v2 API",
-    "user": "'+$AUTHOR+'",
-    "timestamp": "'+$TIMESTAMP+'"
-  }
-}'
+# curl -X POST "https://api.eu.newrelic.com/v2/applications/$NEWRELIC_APP_ID/deployments.json" \
+#      -H "Api-Key:$NEWRELIC_API_KEY" \
+#      -i \
+#      -H "Content-Type: application/json" \
+#      -d \
+# '{
+#   "deployment": {
+#     "revision": "REVISION",
+#     "description": "Added a deployments resource to the v2 API",
+#     "user": "'+$AUTHOR+'",
+#     "timestamp": "'+$TIMESTAMP+'"
+#   }
+# }'
