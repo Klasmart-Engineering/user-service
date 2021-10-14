70186cb0cbca5a11fc76d267b56344cb56d42209
olivermharrison <olivermharrison@gmail.com>
2021-10-14 15:25:24 +0100

    feat: add raw>entity typeorm convernsion util

--- src/utils/typeorm.ts
+++ src/utils/typeorm.ts
@@ -1,4 +1,12 @@
-import { BaseEntity, EntityMetadata, SelectQueryBuilder } from 'typeorm'
+import {
+    BaseEntity,
+    EntityMetadata,
+    getConnection,
+    SelectQueryBuilder,
+} from 'typeorm'
+import { RelationIdLoader } from 'typeorm/query-builder/relation-id/RelationIdLoader'
+import { RelationCountLoader } from 'typeorm/query-builder/relation-count/RelationCountLoader'
+import { RawSqlResultsToEntityTransformer } from 'typeorm/query-builder/transformer/RawSqlResultsToEntityTransformer'
 
 export function scopeHasJoin<E extends BaseEntity>(
     scope: SelectQueryBuilder<E>,
@@ -9,4 +17,38 @@ export function scopeHasJoin<E extends BaseEntity>(
             (joinAttribute) => joinAttribute.metadata?.target === joinedEntity
         ) !== undefined
     )
+}
+
+export async function convertRawToEntities<Entity = unknown>(
+    raw: unknown[],
+    queryBuilder: SelectQueryBuilder<Entity>
+) {
+    const connection = getConnection()
+    const queryRunner = connection.createQueryRunner()
+    const relationIdLoader = new RelationIdLoader(
+        connection,
+        queryRunner,
+        queryBuilder.expressionMap.relationIdAttributes
+    )
+    const relationCountLoader = new RelationCountLoader(
+        connection,
+        queryRunner,
+        queryBuilder.expressionMap.relationCountAttributes
+    )
+    const rawRelationIdResults = await relationIdLoader.load(raw)
+    const rawRelationCountResults = await relationCountLoader.load(raw)
+
+    const transformer = new RawSqlResultsToEntityTransformer(
+        queryBuilder.expressionMap,
+        connection.driver,
+        rawRelationIdResults,
+        rawRelationCountResults,
+        queryRunner
+    )
+
+    const entities: Entity[] = transformer.transform(
+        raw,
+        queryBuilder.expressionMap.mainAlias!
+    )
+    return entities
 }
