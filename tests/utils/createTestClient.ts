53bcf405bf224b068a371677b40fb9174269f611
Oliver Harrison <oliver.harrison@kidsloop.live>
2021-10-18 16:10:22 +0000

    Merged in add-test-logger (pull request #616)
    test: add the express logging middleware to the Apollo test client
    
    * test: add the express logging middleware to createTestClient()
    
    
    Approved-by: Richard Sommerville
    Approved-by: Matthew Carey

--- tests/utils/createTestClient.ts
+++ tests/utils/createTestClient.ts
@@ -17,6 +17,8 @@ import httpMocks, {
     ResponseOptions,
     Headers,
 } from 'node-mocks-http'
+import logger from '../../src/logging'
+import { loggerMiddlewareFactory } from '../../src/middlewares'
 
 type StringOrAst = string | DocumentNode
 
@@ -125,6 +127,7 @@ export const createTestClient = async (
 ): Promise<ApolloServerTestClient> => {
     const app = express()
     app.use(graphqlUploadExpress({ maxFileSize: 2000000, maxFiles: 1 }))
+    app.use(loggerMiddlewareFactory(logger))
     await server.start()
     server.applyMiddleware({ app })
 
