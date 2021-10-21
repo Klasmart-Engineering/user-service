3b0f41d768a34c8c6553489665db690b2f3b81ce
olivermharrison <olivermharrison@gmail.com>
2021-10-21 15:07:09 +0100

    chore: add util function for converting raw sql results to typeorm entities

--- src/utils/typeorm.ts
+++ src/utils/typeorm.ts
@@ -1,4 +1,13 @@
-import { BaseEntity, EntityMetadata, SelectQueryBuilder } from 'typeorm'
+import {
+    BaseEntity,
+    EntityMetadata,
+    getConnection,
+    SelectQueryBuilder,
+} from 'typeorm'
+import { RelationIdLoader } from 'typeorm/query-builder/relation-id/RelationIdLoader'
+import { RelationCountLoader } from 'typeorm/query-builder/relation-count/RelationCountLoader'
+import { RawSqlResultsToEntityTransformer } from 'typeorm/query-builder/transformer/RawSqlResultsToEntityTransformer'
+import { Alias } from 'typeorm/query-builder/Alias'
 
 export function scopeHasJoin<E extends BaseEntity>(
     scope: SelectQueryBuilder<E>,
@@ -9,4 +18,57 @@ export function scopeHasJoin<E extends BaseEntity>(
             (joinAttribute) => joinAttribute.metadata?.target === joinedEntity
         ) !== undefined
     )
+}
+
+export async function convertRawToEntities<Entity = unknown>(
+    raw: unknown[],
+    queryBuilder: SelectQueryBuilder<Entity>
+): Promise<(Entity | undefined)[]> {
+    const connection = getConnection()
+    const queryRunner = connection.createQueryRunner()
+    const relationIdLoader = new RelationIdLoader(
+        connection,
+        queryRunner,
+        queryBuilder.expressionMap.relationIdAttributes
+    )
+    const relationCountLoader = new RelationCountLoader(
+        connection,
+        queryRunner,
+        queryBuilder.expressionMap.relationCountAttributes
+    )
+    const rawRelationIdResults = await relationIdLoader.load(raw)
+    const rawRelationCountResults = await relationCountLoader.load(raw)
+
+    const transformer = new RawSqlResultsToEntityTransformer(
+        queryBuilder.expressionMap,
+        connection.driver,
+        rawRelationIdResults,
+        rawRelationCountResults,
+        queryRunner
+    )
+
+    // convert one-by-one to avoid de-duplication
+    const entities: (Entity | undefined)[] = []
+    for (const row of raw) {
+        const entity = convertRawToEntity<Entity>(
+            row,
+            transformer,
+            queryBuilder.expressionMap.mainAlias!
+        )
+        entities.push(entity)
+    }
+
+    return entities
+}
+
+function convertRawToEntity<Entity = unknown>(
+    raw: unknown,
+    transformer: RawSqlResultsToEntityTransformer,
+    alias: Alias
+): Entity | undefined {
+    const entities: Entity[] = transformer.transform([raw], alias)
+    if (entities.length === 0) {
+        return undefined
+    }
+    return entities[0]
 }
---
+++ tests/integration/utils/typeorm.test.ts
@@ -0,0 +1,68 @@
+import { expect } from 'chai'
+import { Connection, createQueryBuilder } from 'typeorm'
+import { User } from '../../../src/entities/user'
+import { convertRawToEntities } from '../../../src/utils/typeorm'
+import { createUser } from '../../factories/user.factory'
+import { createTestConnection } from '../../utils/testConnection'
+
+describe('convertRawToEntities', () => {
+    let connection: Connection
+
+    before(async () => {
+        connection = await createTestConnection()
+    })
+
+    after(async () => {
+        await connection?.close()
+    })
+    it('converts a raw SQL result to typeorm entities', async () => {
+        await createUser().save()
+        const userQuery = createQueryBuilder(User)
+
+        const rawResult = await userQuery.getRawMany()
+        const entities = await convertRawToEntities(rawResult, userQuery)
+
+        expect(entities).to.have.lengthOf(1)
+        for (const user of entities) {
+            expect(user).to.be.instanceOf(User)
+        }
+    })
+
+    it('discards any columns not defined by the entity', async () => {
+        await createUser().save()
+        const userQuery = createQueryBuilder(User).addSelect(
+            'given_name',
+            'column_to_discard'
+        )
+
+        const rawResult = await userQuery.getRawMany()
+        const entities = await convertRawToEntities(rawResult, userQuery)
+
+        expect(entities).to.have.lengthOf(1)
+        for (const user of entities) {
+            expect((user as any)['column_to_discard']).to.be.undefined
+        }
+    })
+    it('returns undefined for invalid objects', async () => {
+        await createUser().save()
+        const userQuery = createQueryBuilder(User)
+
+        const entities = await convertRawToEntities(
+            [{ random: 'object' }],
+            userQuery
+        )
+        expect(entities).to.have.lengthOf(1)
+        expect(entities[0]).to.be.undefined
+    })
+    it('does not deduplicate', async () => {
+        await createUser().save()
+        const userQuery = createQueryBuilder(User)
+        const rawResult = await userQuery.getRawMany()
+
+        const entities = await convertRawToEntities(
+            [...rawResult, ...rawResult],
+            userQuery
+        )
+        expect(entities).to.have.lengthOf(2)
+    })
+})
