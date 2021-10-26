d1d9263c7070e87b7950c3b24e95c36e1fb02031
olivermharrison <olivermharrison@gmail.com>
2021-10-26 13:11:37 +0100

    perf: store requests in a map and use a string hash for their keys

--- src/loaders/childConnectionLoader.ts
+++ src/loaders/childConnectionLoader.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto'
 import { BaseEntity, getManager, SelectQueryBuilder } from 'typeorm'
 import { filterHasProperty } from '../utils/pagination/filtering'
 import {
@@ -41,7 +42,7 @@ export interface IChildConnectionDataloaderKey<
 // }
 function getRequestIdentifier(key: IChildConnectionDataloaderKey<BaseEntity>) {
     // flatten the object and discard the scope property and unique parentId
-    return JSON.stringify({
+    const flatObj = {
         direction: key.args.direction,
         count: key.args.count,
         cursor: key.args.cursor,
@@ -50,14 +51,16 @@ function getRequestIdentifier(key: IChildConnectionDataloaderKey<BaseEntity>) {
         includeTotalCount: key.includeTotalCount,
         parentPivot: key.parent.pivot,
         parentFilterKey: key.parent.filterKey,
-    })
+    }
+    return crypto
+        .createHash('md5')
+        .update(JSON.stringify(flatObj))
+        .digest('hex')
 }
 
 interface IChildConnectionRequest<Node> {
     // the unique request properties
     key: IChildConnectionDataloaderKey<BaseEntity>
-    // a generated ID to identify this request
-    stringId: string
     // the list of parent Ids to filter by (e.g. orgIds in this case)
     parentIds: string[]
     // the final parentId:childConnections map for this unique request
@@ -77,198 +80,206 @@ export const childConnectionLoader = async <Entity extends BaseEntity, Node>(
     }
 
     // group keys by request as described above
-    const requests: IChildConnectionRequest<Node>[] = []
+    const requests: Map<string, IChildConnectionRequest<Node>> = new Map()
     for (const key of keys) {
         const id = getRequestIdentifier(key)
-        const request = requests.find((r) => getRequestIdentifier(r.key) === id)
-        if (!request) {
-            requests.push({
-                stringId: id,
+        const request = requests.get(id)
+        if (request) {
+            request.parentIds.push(key.parent.id)
+        } else {
+            requests.set(id, {
                 key,
                 parentIds: [key.parent.id],
                 result: new Map(),
             })
-        } else {
-            request.parentIds.push(key.parent.id)
         }
     }
 
-    // resolve all child connections independently per request
-    for (const request of requests) {
-        // there may be duplicate parentIds, so use a unique set to create maps
-        const parentIds = request.parentIds
-        const uniqueParentIds = [...new Set(parentIds)]
+    // resolve all child connections independently per request, in parallel
+    await Promise.all(
+        Array.from(requests.entries()).map(async ([id, request]) => {
+            // there may be duplicate parentIds, so use a unique set to create maps
+            const parentIds = request.parentIds
+            const uniqueParentIds = [...new Set(parentIds)]
 
-        const args = request.key.args as IChildPaginationArgs<Entity>
-        const parent = request.key.parent
-        const includeTotalCount = request.key.includeTotalCount
-        const groupByProperty = parent.pivot
-        const pivotColumn = 'pivot'
+            const args = request.key.args as IChildPaginationArgs<Entity>
+            const parent = request.key.parent
+            const includeTotalCount = request.key.includeTotalCount
+            const groupByProperty = parent.pivot
+            const pivotColumn = 'pivot'
 
-        // not allowed to filter by the parent entity
-        if (args.filter && filterHasProperty(parent.filterKey, args.filter)) {
-            throw new Error(
-                `Cannot filter by parent ID ${parent.filterKey} in a child connection.`
-            )
-        }
+            // not allowed to filter by the parent entity
+            if (
+                args.filter &&
+                filterHasProperty(parent.filterKey, args.filter)
+            ) {
+                throw new Error(
+                    `Cannot filter by parent ID ${parent.filterKey} in a child connection.`
+                )
+            }
 
-        // Create the Dataloader map of parentId: childConnectionNode[]
-        const parentMap = new Map<string, IPaginatedResponse<Node>>(
-            uniqueParentIds.map((parentId) => [
-                parentId,
-                {
-                    totalCount: 0,
-                    pageInfo: {
-                        hasPreviousPage: true,
-                        hasNextPage: true,
-                        startCursor: '',
-                        endCursor: '',
+            // Create the Dataloader map of parentId: childConnectionNode[]
+            const parentMap = new Map<string, IPaginatedResponse<Node>>(
+                uniqueParentIds.map((parentId) => [
+                    parentId,
+                    {
+                        totalCount: 0,
+                        pageInfo: {
+                            hasPreviousPage: true,
+                            hasNextPage: true,
+                            startCursor: '',
+                            endCursor: '',
+                        },
+                        edges: [],
                     },
-                    edges: [],
-                },
-            ])
-        )
-
-        // Create the base scope with necessary joins, filters, and selects
-        // Sorting and pagination is done separately in the paginationScope
-        const baseScope = await connectionQuery({
-            direction: args.direction || 'FORWARD',
-            directionArgs: {
-                cursor: args.cursor,
-            },
-            scope: args.scope,
-            filter: {
-                [parent.filterKey]: {
-                    operator: 'in',
-                    value: uniqueParentIds,
-                },
-                ...args.filter,
-            },
-        })
-
-        //
-        // Get the total counts per parent and update the dataloader map
-        //
-        if (includeTotalCount) {
-            // Create the query to get total children counts per parent (ignores pagination filters etc)
-            const countScope = baseScope
-                .clone()
-                .select([`${groupByProperty} as "${pivotColumn}"`, 'count(*)'])
-                .addSelect('count(*)')
-                .groupBy(`"${pivotColumn}"`)
-
-            // Get the counts and update the dataloader map
-            const parentCounts: {
-                [pivotColumn]: string
-                count: string
-            }[] = await countScope.getRawMany()
-            for (const parentCount of parentCounts) {
-                const children = parentMap.get(parentCount[pivotColumn])
-                if (children) {
-                    children.totalCount = parseInt(parentCount.count)
-                }
-            }
-        }
-
-        //
-        // Get the children per parent and update the dataloader map
-        //
-
-        // apply pagination where clause and order by statements to the scope
-        const {
-            scope: paginationScope,
-            pageSize,
-            primaryColumns,
-            cursorData,
-        } = await getPaginationQuery({
-            direction: args.direction || 'FORWARD',
-            directionArgs: {
-                count: args.count,
-                cursor: args.cursor,
-            },
-            scope: baseScope.clone(),
-            sort: {
-                ...sort,
-                sort: args.sort,
-            },
-            includeTotalCount: false,
-        })
-        // get one more item to determine if there is another page
-        const seekPageSize = pageSize + 1
-
-        // Select the row number to select n _sorted_ children per parent,
-        // respecting the requested order
-        const filterQuery = paginationScope.getQuery()
-        // TODO a better way of extracting the ORDER BY clause
-        const orderBy = filterQuery.slice(filterQuery.indexOf('ORDER BY'))
-
-        paginationScope.addSelect(`${groupByProperty} as "${pivotColumn}"`)
-        paginationScope.addSelect(
-            `ROW_NUMBER() OVER (PARTITION BY ${groupByProperty} ${orderBy})`,
-            'row_num'
-        )
-
-        // Create the query for getting n children per parents
-        const childScope = getManager()
-            .createQueryBuilder()
-            .select('*')
-            .from(`(${paginationScope.getQuery()})`, 'subquery')
-            .where(`"row_num" <= ${seekPageSize}`)
-            .setParameters(paginationScope.getParameters())
-
-        // get raw SQL results and create a map of parentId:childEntity
-        const childrenRaw: {
-            [pivotColumn]: string
-        }[] = await childScope.getRawMany()
-        const entities = await convertRawToEntities(childrenRaw, baseScope)
-
-        const parentToChildMap = new Map<string, Entity[]>(
-            uniqueParentIds.map((id) => [id, []])
-        )
-        childrenRaw.forEach((childRaw, index) => {
-            const entity = entities[index]
-            if (entity) {
-                const parentId = childRaw[pivotColumn]
-                const children = parentToChildMap.get(parentId)
-                children?.push(entity)
-                parentToChildMap.set(parentId, children || [])
-            }
-        })
-
-        // for each parent, calculate their edges and page info
-        for (const [parentId, children] of parentToChildMap) {
-            const { pageInfo, edges } = getPageInfoAndEdges<Entity>(
-                children,
-                pageSize,
-                sort.primaryKey,
-                primaryColumns,
-                parentMap.get(parentId)?.totalCount ?? 0,
-                cursorData,
-                args.direction
+                ])
             )
 
-            const parentResult = parentMap.get(parentId)
-            if (parentResult) {
-                for (const edge of edges) {
-                    const processedEdge = {
-                        cursor: edge.cursor,
-                        node: await entityToNodeMapFunction(edge.node),
-                    }
-                    parentResult.edges.push(processedEdge)
-                }
-                parentResult.pageInfo = pageInfo
-            }
-        }
+            // Create the base scope with necessary joins, filters, and selects
+            // Sorting and pagination is done separately in the paginationScope
+            const baseScope = await connectionQuery({
+                direction: args.direction || 'FORWARD',
+                directionArgs: {
+                    cursor: args.cursor,
+                },
+                scope: args.scope,
+                filter: {
+                    [parent.filterKey]: {
+                        operator: 'in',
+                        value: uniqueParentIds,
+                    },
+                    ...args.filter,
+                },
+            })
 
-        request.result = parentMap
-    }
+            //
+            // Get the total counts per parent and update the dataloader map
+            //
+            if (includeTotalCount) {
+                // Create the query to get total children counts per parent (ignores pagination filters etc)
+                const countScope = baseScope
+                    .clone()
+                    .select([
+                        `${groupByProperty} as "${pivotColumn}"`,
+                        'count(*)',
+                    ])
+                    .addSelect('count(*)')
+                    .groupBy(`"${pivotColumn}"`)
+
+                // Get the counts and update the dataloader map
+                const parentCounts: {
+                    [pivotColumn]: string
+                    count: string
+                }[] = await countScope.getRawMany()
+                for (const parentCount of parentCounts) {
+                    const children = parentMap.get(parentCount[pivotColumn])
+                    if (children) {
+                        children.totalCount = parseInt(parentCount.count)
+                    }
+                }
+            }
+
+            //
+            // Get the children per parent and update the dataloader map
+            //
+
+            // apply pagination where clause and order by statements to the scope
+            const {
+                scope: paginationScope,
+                pageSize,
+                primaryColumns,
+                cursorData,
+            } = await getPaginationQuery({
+                direction: args.direction || 'FORWARD',
+                directionArgs: {
+                    count: args.count,
+                    cursor: args.cursor,
+                },
+                scope: baseScope.clone(),
+                sort: {
+                    ...sort,
+                    sort: args.sort,
+                },
+                includeTotalCount: false,
+            })
+            // get one more item to determine if there is another page
+            const seekPageSize = pageSize + 1
+
+            // Select the row number to select n _sorted_ children per parent,
+            // respecting the requested order
+            const filterQuery = paginationScope.getQuery()
+            // TODO a better way of extracting the ORDER BY clause
+            const orderBy = filterQuery.slice(filterQuery.indexOf('ORDER BY'))
+
+            paginationScope.addSelect(`${groupByProperty} as "${pivotColumn}"`)
+            paginationScope.addSelect(
+                `ROW_NUMBER() OVER (PARTITION BY ${groupByProperty} ${orderBy})`,
+                'row_num'
+            )
+
+            // Create the query for getting n children per parents
+            const childScope = getManager()
+                .createQueryBuilder()
+                .select('*')
+                .from(`(${paginationScope.getQuery()})`, 'subquery')
+                .where(`"row_num" <= ${seekPageSize}`)
+                .setParameters(paginationScope.getParameters())
+
+            // get raw SQL results and create a map of parentId:childEntity
+            const childrenRaw: {
+                [pivotColumn]: string
+            }[] = await childScope.getRawMany()
+            const entities = await convertRawToEntities(childrenRaw, baseScope)
+
+            const parentToChildMap = new Map<string, Entity[]>(
+                uniqueParentIds.map((id) => [id, []])
+            )
+            childrenRaw.forEach((childRaw, index) => {
+                const entity = entities[index]
+                if (entity) {
+                    const parentId = childRaw[pivotColumn]
+                    const children = parentToChildMap.get(parentId)
+                    children?.push(entity)
+                    parentToChildMap.set(parentId, children || [])
+                }
+            })
+
+            // for each parent, calculate their edges and page info
+            for (const [parentId, children] of parentToChildMap) {
+                const { pageInfo, edges } = getPageInfoAndEdges<Entity>(
+                    children,
+                    pageSize,
+                    sort.primaryKey,
+                    primaryColumns,
+                    parentMap.get(parentId)?.totalCount ?? 0,
+                    cursorData,
+                    args.direction
+                )
+
+                const parentResult = parentMap.get(parentId)
+                if (parentResult) {
+                    for (const edge of edges) {
+                        const processedEdge = {
+                            cursor: edge.cursor,
+                            node: await entityToNodeMapFunction(edge.node),
+                        }
+                        parentResult.edges.push(processedEdge)
+                    }
+                    parentResult.pageInfo = pageInfo
+                }
+            }
+
+            request.result = parentMap
+        })
+    )
 
     // finally, map the request results to the dataloader input keys
     const finalResult: IPaginatedResponse<Node>[] = []
     for (const key of keys) {
         // find the request
         const id = getRequestIdentifier(key)
-        const request = requests.find((r) => r.stringId === id)
+        // const request = requests.find((r) => r.stringId === id)
+        const request = requests.get(id)
 
         // get the result for the requested parentId
         const result = request?.result.get(key.parent.id)
