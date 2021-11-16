a09db177bc57793ce6851c8b27abfd4ce8ca5e96
Edgardo Martinez <edgardom@bluetrailsoft.com>
2021-11-16 16:57:57 -0300

    - Comments added in the code for explain the scenario. - Set the default data for the scenario. - Ticket related: https://calmisland.atlassian.net/browse/UD-1650

--- tests/performance/src/Scenarios/Scenario 9/scenario9ASchoolAdmin.ts
+++ tests/performance/src/Scenarios/Scenario 9/scenario9ASchoolAdmin.ts
@@ -2,6 +2,30 @@ import { Options } from "k6/options";
 import loginSetup from "../../utils/loginSetup";
 import getUsers from "../../scripts/getOrganizationUsers";
 
+/* 
+Link URL= https://calmisland.atlassian.net/wiki/spaces/BTS/pages/edit-v2/2398781836
+    - See: Scenario 9 - As a School Admin → lands on Users
+    - Ticket: UD-1650 
+
+This is scenario consist of:
+    - Create Script for Login multiple School Admin with unique accounts.
+    - The script should emulate that each School Admin lands on the Home Page.
+    - Then the School Admin can go to:
+        - Users & set the page per row in 10
+        - Accounts: 5 uniques account
+*/
+
+/* 
+- Run with a max of VUs = 1050
+    - The number of users starts at 0, and slowly ramps up to the nominal value (20 secs-115 max), 
+        where it stays for an extended period of time (3m). Then a ramp downstage goes to 0 in 20 secs.
+    - Stage 1-  ramps up: duration: '20s' - target: 5 
+    - Stage 2 - keep the load : duration: '3m' - target: 50
+    - Stage3 ramp downstage : duration: '20s' - target: 0
+    -Total time:  2m45s
+*/
+
+
 export const options: Options = {
     scenarios: {
         school00: {
@@ -18,7 +42,7 @@ export const options: Options = {
                 // Hold
                 {
                     duration: '2m',
-                    target: 300
+                    target: 200
                 },
                 // Ramp down
                 {
@@ -36,7 +60,7 @@ export const options: Options = {
                 // Ramp up               
                 {
                     duration: '20s',
-                    target: 7
+                    target: 5
                 },
                 // Hold
                 {
@@ -59,12 +83,12 @@ export const options: Options = {
                 // Ramp up               
                 {
                     duration: '20s',
-                    target: 8
+                    target: 5
                 },
                 // Hold
                 {
                     duration: '2m',
-                    target: 300
+                    target: 200
                 },
                 // Ramp down
                 {
@@ -87,7 +111,7 @@ export const options: Options = {
                 // Hold
                 {
                     duration: '2m',
-                    target: 300
+                    target: 200
                 },
                 // Ramp down
                 {
@@ -105,12 +129,12 @@ export const options: Options = {
                 // Ramp up               
                 {
                     duration: '20s',
-                    target: 7
+                    target: 5
                 },
                 // Hold
                 {
                     duration: '2m',
-                    target: 300
+                    target: 200
                 },
                 // Ramp down
                 {
@@ -133,7 +157,7 @@ export const options: Options = {
                 // Hold
                 {
                     duration: '2m',
-                    target: 300
+                    target: 200
                 },
                 // Ramp down
                 {
