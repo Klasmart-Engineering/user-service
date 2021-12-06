af8aa0de72f656be8788cfddb353ea2b562cb642
Edgardo Martinez <edgardom@bluetrailsoft.com>
2021-12-06 15:00:20 -0300

    Adding the scheduleFilterPrograms.

--- tests/performance/src/scripts/scheduleFilterProgram.ts
+++ tests/performance/src/scripts/scheduleFilterProgram.ts
@@ -1,34 +1,32 @@
 import { check } from 'k6';
 import http from 'k6/http';
 import { Options } from 'k6/options';
-import { meQuery } from '../queries/users';
+//import { meQuery } from '../queries/users';
 
 
 export const options:Options = {
     vus: 1,
 };
 
-const params = {
+/* const params = {
     headers: {
         'Content-Type': `application/json`,
     },
-};
+}; */
 
 export default function (roleType?: string) {
-    const userPayload = JSON.stringify(
+    /* const userPayload = JSON.stringify(
         {
         variables: {},
-        query: meQuery,
-    })
+    }) */
 
+   const res = http.get(`${process.env.SCHEDULE_FILTER_PROGRAM_URL}?org_id=${process.env.ORG_ID}` as string);
 
-   const res = http.post(`${process.env.SCHEDULE_FILTER_PROGRAM_URL}?${process.env.ORG_ID}` as string, userPayload, params);
-
-    console.log(JSON.stringify(res));
+  //  console.log(JSON.stringify(res));
 
     check(res, {
         'SCHEDULE_FILTER_Program - status is 200': () => res.status === 200,
-        //'schedule endpoint returns data': (r) => JSON.parse(r.body as string).data,
+        //'schedule endpoint returns data': (r) => JSON.parse(r.body as string).data.id ?? false,
     }, {
         userRoleType: roleType
     });
