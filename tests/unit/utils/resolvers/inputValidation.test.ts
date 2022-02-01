74b0b2e35fe9185e6425f73812a0b03430fa2985
Marlon Torres <marlont@bluetrailsoft.com>
2022-02-01 15:09:29 +0000

    fix: removing unecessary server creation
    * fix: removing createServer line in test
    
    
    Approved-by: Richard Sommerville
    Approved-by: Toghrul Taghiyev
    Approved-by: Matthew Carey

--- tests/unit/utils/resolvers/inputValidation.test.ts
+++ tests/unit/utils/resolvers/inputValidation.test.ts
@@ -35,8 +35,6 @@ import {
     TestConnection,
 } from '../../../utils/testConnection'
 import { AgeRange } from '../../../../src/entities/ageRange'
-import { createServer } from '../../../../src/utils/createServer'
-import { Model } from '../../../../src/model'
 import { createAgeRanges } from '../../../factories/ageRange.factory'
 
 describe('inputValidation', () => {
