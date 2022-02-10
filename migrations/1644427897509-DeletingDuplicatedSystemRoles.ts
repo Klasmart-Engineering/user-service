b22123623254b7c56a9c5cd244784b35d3e47f54
Marlon Torres <marlont@bluetrailsoft.com>
2022-02-10 13:51:12 -0600

    test(AD-1659): adding migration tests

--- migrations/1644427897509-DeletingDuplicatedSystemRoles.ts
+++ migrations/1644427897509-DeletingDuplicatedSystemRoles.ts
@@ -2,48 +2,54 @@ import {
     EntityManager,
     In,
     MigrationInterface,
+    Not,
     QueryRunner,
     WhereExpression,
 } from 'typeorm'
 import { OrganizationMembership } from '../src/entities/organizationMembership'
 import { Role } from '../src/entities/role'
 import { SchoolMembership } from '../src/entities/schoolMembership'
-import { organizationAdminRole } from '../src/permissions/organizationAdmin'
-import { parentRole } from '../src/permissions/parent'
-import { schoolAdminRole } from '../src/permissions/schoolAdmin'
-import { studentRole } from '../src/permissions/student'
-import { teacherRole } from '../src/permissions/teacher'
+import logger from '../src/logging'
+
+// Since this migration is just for remove duplications in an specific environment
+// we are removing the exact roles that has not the correct permissions
+export const rolesToDeleteIds = [
+    '23d899cd-862e-4bb6-8e57-761d701bc9fb' /* Organization Admin */,
+    'f70038dd-0361-4f19-8d50-5e42997a6280' /* School Admin */,
+    'f1a2083a-7f77-4ec8-b64a-def6952bf807' /* Parent */,
+    'fd37310d-5ced-4dda-9968-c6cb084a542b' /* Student */,
+    '893f28d2-69f1-4dc5-aa37-a04b9a8d90b9' /* Teacher */,
+]
 
 export class DeletingDuplicatedSystemRoles1644427897509
     implements MigrationInterface {
-    private systemRolesData = [
-        organizationAdminRole,
-        schoolAdminRole,
-        parentRole,
-        studentRole,
-        teacherRole,
-    ]
-
     public async up(queryRunner: QueryRunner): Promise<void> {
         const manager = queryRunner.manager
-        const systemRoleNames = this.systemRolesData.map((r) => r.role_name)
-        const systemRoles = await manager.find(Role, {
-            where: {
-                role_name: In(systemRoleNames),
-                system_role: true,
-            },
-            order: {
-                created_at: 'ASC',
-            },
-        })
+        // Searching for the roles that will be deleted
+        const rolesToDelete = await this.getRolesMapByIds(
+            manager,
+            'in',
+            rolesToDeleteIds,
+            'role_id'
+        )
 
-        if (systemRoles.length > systemRoleNames.length) {
+        // Searching for the roles that will replace the deleted ones
+        const rolesToPersist = await this.getRolesMapByIds(
+            manager,
+            'not-in',
+            rolesToDeleteIds,
+            'role_name'
+        )
+
+        if (
+            rolesToDelete.size === rolesToDeleteIds.length &&
+            rolesToPersist.size === rolesToDeleteIds.length
+        ) {
+            // Getting the memberships involved in roles to delete
             const {
-                rolesToDelete,
-                rolesToPersist,
                 orgMemberships,
                 schoolMemberships,
-            } = await this.buildMaps(manager, systemRoles)
+            } = await this.getMembershipsToMigrate(manager)
 
             // Migrating roles to be removed to the persisting ones in the org memberships involved
             await this.migrateMemberships(
@@ -71,33 +77,33 @@ export class DeletingDuplicatedSystemRoles1644427897509
     }
 
     public async down(_queryRunner: QueryRunner): Promise<void> {
-        throw new Error(
+        logger.warn(
             'This is a data migration for remove duplications. Down the migration would mean create duplications'
         )
     }
 
-    private async buildMaps(manager: EntityManager, systemRoles: Role[]) {
-        const rolesToDelete = new Map<string, Role[]>()
-        const rolesToPersist = new Map<string, Role>()
-
-        systemRoles.forEach((r) => {
-            const roleName = r.role_name!
-
-            if (rolesToPersist.has(roleName)) {
-                if (rolesToDelete.has(roleName)) {
-                    rolesToDelete.get(roleName)?.push(r)
-                } else {
-                    rolesToDelete.set(roleName, [r])
-                }
-            } else {
-                rolesToPersist.set(roleName, r)
-            }
+    private async getRolesMapByIds(
+        manager: EntityManager,
+        operator: 'in' | 'not-in',
+        ids: string[],
+        key: 'role_id' | 'role_name'
+    ) {
+        const preloadedRolesToDelete = manager.find(Role, {
+            where: {
+                role_id: operator === 'in' ? In(ids) : Not(In(ids)),
+                system_role: true,
+            },
+            order: {
+                created_at: 'ASC',
+            },
         })
 
-        const rolesToDeleteIds = Array.from(rolesToDelete.values())
-            .map((roles) => roles.map((r) => r.role_id))
-            .flat()
+        return new Map<string, Role>(
+            (await preloadedRolesToDelete).map((r) => [r[key]!, r])
+        )
+    }
 
+    private async getMembershipsToMigrate(manager: EntityManager) {
         const orgMemberships = await manager.find(OrganizationMembership, {
             join: {
                 alias: 'OrgMembership',
@@ -127,8 +133,6 @@ export class DeletingDuplicatedSystemRoles1644427897509
         })
 
         return {
-            rolesToDelete,
-            rolesToPersist,
             orgMemberships,
             schoolMemberships,
         }
@@ -137,7 +141,7 @@ export class DeletingDuplicatedSystemRoles1644427897509
     private async migrateMemberships(
         manager: EntityManager,
         memberships: OrganizationMembership[] | SchoolMembership[],
-        rolesToDelete: Map<string, Role[]>,
+        rolesToDelete: Map<string, Role>,
         rolesToPersist: Map<string, Role>
     ) {
         for (const m of memberships) {
@@ -147,10 +151,13 @@ export class DeletingDuplicatedSystemRoles1644427897509
             for (const r of roles) {
                 let roleToPush = r
                 if (r.role_name) {
-                    const roleToDelete = rolesToDelete.get(r.role_name)
+                    const roleToDelete = rolesToDelete.get(r.role_id)
 
                     if (roleToDelete) {
-                        const roleToReplace = rolesToPersist.get(r.role_name)!
+                        const roleToReplace = rolesToPersist.get(
+                            roleToDelete.role_name!
+                        )!
+
                         roleToPush = roleToReplace
                     }
                 }
