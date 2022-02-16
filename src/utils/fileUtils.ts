d6088de9e0fd657bbd9ab5d31ed701fb36f751c4
Richard Sommerville <richard.sommerville@kidsloop.live>
2022-02-16 08:52:02 +0000

    chore(AD-1903): code coverage in CI and fix acceptance test coverage
    * chore: output text and JSON versions of acceptance test coverage
    
    * chore: move test coverage collection out of main.ts
    
    * chore: fix coverage commands to work with --all
    
    https://github.com/istanbuljs/nyc/issues/679#issuecomment-345602235
    
    * chore: remove code coverage badges
    
    https://kidsloop.slack.com/archives/C01T8EZ6Z08/p1644318675409469
    
    * chore(AD-1903): code coverage in CI and fix acceptance test coverage
    
    When we move to github we want to use codecov to report on coverage
    Codecov expects you to send it pre-existing coverage reports
    They support the lcov.info format this setup will produce…
    
    Approved-by: Oliver Harrison
    Approved-by: Toghrul Taghiyev

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -1,4 +1,6 @@
 import { FileUpload } from 'graphql-upload'
+import logger from '../logging'
+import fs from 'fs'
 
 export function extensionForFile(file: FileUpload): string | undefined {
     const mimetype = file.mimetype
@@ -10,4 +12,29 @@ export function extensionForFile(file: FileUpload): string | undefined {
         fileExt = matchRes[0]
     }
     return fileExt
+}
+
+// If the src code has been instrumented with istanbul
+// then the results will be stored in global.__coverage__
+// see https://stackoverflow.com/a/59626976
+// and https://github.com/kirksl/karate-istanbul/blob/e5eecd7596b5c2087a07cd61aff6b2025d6c850d/test/test.js#L21
+export function saveCodeCoverage() {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const coverageResults = (<any>global).__coverage__
+    if (coverageResults !== undefined) {
+        if (process.env.NODE_ENV === 'development') {
+            logger.info('code coverage information found, saving')
+        } else {
+            logger.warn(
+                `code coverage results found with NODE_ENV of ${process.env.NODE_ENV}`
+            )
+        }
+        fs.writeFileSync(
+            './.nyc_output/container_coverage.json',
+            JSON.stringify(coverageResults),
+            {
+                encoding: 'utf8',
+            }
+        )
+    }
 }
