Klasmart-Engineering/test-us-repo

perf: perf tests

tests/integration/utils/csv/importEntity.test.ts
@@ -16,6 +16,15 @@ import {
     createTestClient,
 } from '../../../utils/createTestClient'
 import { createAdminUser } from '../../../utils/testEntities'
+import { processUserFromCSVRows } from '../../../../src/utils/csv/user'
+import { createOrganization } from '../../../factories/organization.factory'
+import { createSchool } from '../../../factories/school.factory'
+import { createClass } from '../../../factories/class.factory'
+import { Role } from '../../../../src/entities/role'
+import { createUser, createUsers } from '../../../factories/user.factory'
+import { createOrganizationMembership } from '../../../factories/organizationMembership.factory'
+import { User } from '../../../../src/entities/user'
+import { userToPayload } from '../../../utils/operations/userOps'
 
 use(chaiAsPromised)
 
@@ -190,5 +199,100 @@ describe('createEntityFromCsvWithRollBack', () => {
                 .count()
             expect(organizationCount).eq(0)
         })
+    })
+
+    context.only('perf', () => {
+        const fileName = 'usersPerfExample.csv'
+        let fileStream: fs.ReadStream
+        let uploader: User
+
+        for (let i = 0; i < 1; i++) {
+            beforeEach(async () => {
+                fileStream = fs.createReadStream(
+                    `tests/fixtures/${fileName}`,
+                    'utf-8'
+                )
+
+                file = {
+                    filename: fileName,
+                    mimetype: 'text/csv',
+                    encoding: '7bit',
+                    createReadStream: () => fileStream,
+                }
+
+                const organization = createOrganization()
+                organization.organization_name = 'Chrysalis Digital'
+                await organization.save()
+                const school = await createSchool(
+                    organization,
+                    'Chrysalis Golden Ticket'
+                ).save()
+                await createClass(
+                    [school],
+                    organization,
+                    {},
+                    'Golden Ticket Class'
+                ).save()
+                const teacher = await connection
+                    .createEntityManager()
+                    .findOne(Role, { where: { role_name: 'Teacher' } })
+                const student = await connection
+                    .createEntityManager()
+                    .findOne(Role, { where: { role_name: 'Student' } })
+                const orgAdmin = await connection
+                    .createEntityManager()
+                    .findOne(Role, {
+                        where: { role_name: 'Organization Admin' },
+                    })
+                uploader = await createUser().save()
+                await createOrganizationMembership({
+                    user: uploader,
+                    organization,
+                    roles: [orgAdmin!],
+                }).save()
+                const user = createUsers(1000)
+                await connection.manager.save(user)
+                await connection.manager.save(
+                    user.map((u) =>
+                        createOrganizationMembership({
+                            user: u,
+                            organization,
+                            roles: [teacher!, student!],
+                        })
+                    )
+                )
+
+                for (let i = 0; i < 10; i++) {
+                    // other org we dont care about
+                    const organization2 = await createOrganization().save()
+                    const user2 = createUsers(100)
+                    await connection.manager.save(user2)
+                    await connection.manager.save(
+                        user2.map((u) =>
+                            createOrganizationMembership({
+                                user: u,
+                                organization: organization2,
+                            })
+                        )
+                    )
+                }
+            })
+
+            it(`test ${i}`, async () => {
+                connection.logger.reset()
+                console.log('in test')
+                console.time('csv time')
+                await createEntityFromCsvWithRollBack(
+                    connection,
+                    file,
+                    [processUserFromCSVRows],
+                    new UserPermissions(userToPayload(uploader)),
+                    undefined
+                )
+                console.timeEnd('csv time')
+                console.log('not in test')
+                fs.writeFileSync('./poop', connection.logger.dumpStats)
+            })
+        }
     })
 })

Klasmart-Engineering/user-service

tests/integration/utils/csv/importEntity.test.ts
@@ -16,6 +16,15 @@ import {
     createTestClient,
 } from '../../../utils/createTestClient'
 import { createAdminUser } from '../../../utils/testEntities'
+import { processUserFromCSVRows } from '../../../../src/utils/csv/user'
+import { createOrganization } from '../../../factories/organization.factory'
+import { createSchool } from '../../../factories/school.factory'
+import { createClass } from '../../../factories/class.factory'
+import { Role } from '../../../../src/entities/role'
+import { createUser, createUsers } from '../../../factories/user.factory'
+import { createOrganizationMembership } from '../../../factories/organizationMembership.factory'
+import { User } from '../../../../src/entities/user'
+import { userToPayload } from '../../../utils/operations/userOps'
 
 use(chaiAsPromised)
 
@@ -189,6 +198,95 @@ describe('createEntityFromCsvWithRollBack', () => {
                 .getRepository(Organization)
                 .count()
             expect(organizationCount).eq(0)
+        })
+    })
+
+    context.only('perf', () => {
+        const fileName = 'usersPerfExample.csv'
+        let fileStream: fs.ReadStream
+        let uploader: User
+
+        beforeEach(async () => {
+            fileStream = fs.createReadStream(
+                `tests/fixtures/${fileName}`,
+                'utf-8'
+            )
+
+            file = {
+                filename: fileName,
+                mimetype: 'text/csv',
+                encoding: '7bit',
+                createReadStream: () => fileStream,
+            }
+
+            const organization = createOrganization()
+            organization.organization_name = 'Chrysalis Digital'
+            await organization.save()
+            const school = await createSchool(
+                organization,
+                'Chrysalis Golden Ticket'
+            ).save()
+            await createClass(
+                [school],
+                organization,
+                {},
+                'Golden Ticket Class'
+            ).save()
+            const teacher = await connection
+                .createEntityManager()
+                .findOne(Role, { where: { role_name: 'Teacher' } })
+            const student = await connection
+                .createEntityManager()
+                .findOne(Role, { where: { role_name: 'Student' } })
+            const orgAdmin = await connection
+                .createEntityManager()
+                .findOne(Role, {
+                    where: { role_name: 'Organization Admin' },
+                })
+            uploader = await createUser().save()
+            await createOrganizationMembership({
+                user: uploader,
+                organization,
+                roles: [orgAdmin!],
+            }).save()
+            const users = createUsers(1000)
+            await connection.manager.save(users)
+            await connection.manager.save(
+                users.map((u) =>
+                    createOrganizationMembership({
+                        user: u,
+                        organization,
+                        roles: [teacher!, student!],
+                    })
+                )
+            )
+
+            for (let i = 0; i < 10; i++) {
+                // other org we dont care about
+                const otherOrganization = await createOrganization().save()
+                const otherUsers = createUsers(100)
+                await connection.manager.save(otherUsers)
+                await connection.manager.save(
+                    otherUsers.map((u) =>
+                        createOrganizationMembership({
+                            user: u,
+                            organization: otherOrganization,
+                        })
+                    )
+                )
+            }
+        })
+
+        it('test', async () => {
+            connection.logger.reset()
+            await createEntityFromCsvWithRollBack(
+                connection,
+                file,
+                [processUserFromCSVRows],
+                new UserPermissions(userToPayload(uploader)),
+                undefined
+            )
+            connection.logger.saveSlowQueryStats()
         })
     })
 })