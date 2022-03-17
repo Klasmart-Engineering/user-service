fc192666048e29b390f447ed923952da9edf592d
raphael-piccolin-kidsloop <98455556+raphael-piccolin-kidsloop@users.noreply.github.com>
2022-03-17 14:08:33 +0000

    feat(AD-2225): add age range and subject to csv class creation
    * feat: added age range and subject to csv class creation
    
    * test: added tests for subject and age ranges
    
    * style: used ageRangeName var to avoid repetition
    
    * chore: removed unnecessary query to class table
    
    * fix: re-added classInManager

--- src/types/csv/classRow.ts
+++ src/types/csv/classRow.ts
@@ -5,4 +5,8 @@ export interface ClassRow {
     school_name?: string
     program_name?: string
     grade_name?: string
+    subject_name?: string
+    age_range_low_value?: string
+    age_range_high_value?: string
+    age_range_unit?: string
 }
