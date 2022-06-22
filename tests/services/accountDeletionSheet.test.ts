b88110e10b8fcf41ced7cfd1f0847107b260fa73
Raphael Piccolin <raphael.piccolin@kidsloop.live>
2022-06-22 12:51:29 +0100

    test: moved parseRange() to unit tests

--- tests/services/accountDeletionSheet.test.ts
+++ tests/services/accountDeletionSheet.test.ts
@@ -4,7 +4,6 @@ import { expect } from 'chai'
 import accountDeletionSheet, {
     accountDeletionRequestColumns,
     AccountDeletionRequestRow,
-    parseRange,
 } from '../../src/services/accountDeletionSheet'
 import _ from 'lodash'
 
@@ -47,25 +46,5 @@ describe('accountDeletionSheet', () => {
 
             await accountDeletionSheet.deleteRows(updateRange) // clean up
         })
-    })
-})
-
-describe('parseRange()', () => {
-    it('successfully parses a valid long table range', () => {
-        const rangeString = 'Sheet21412!AC412:POA0123'
-        const parsedRange = parseRange(rangeString)
-        expect(parsedRange.startRow).to.equal(412)
-        expect(parsedRange.endRow).to.equal(123)
-        expect(parsedRange.startColumn).to.equal('AC')
-        expect(parsedRange.endColumn).to.equal('POA')
-    })
-
-    it('throws an error when parsing ranges in an invalid format', () => {
-        expect(() => parseRange('AC412:POA0123')).to.throw(
-            'Failed to parse table range'
-        )
-        expect(() => parseRange('Sheet21412!412AC:123POA')).to.throw(
-            'Failed to parse table range'
-        )
     })
 })
